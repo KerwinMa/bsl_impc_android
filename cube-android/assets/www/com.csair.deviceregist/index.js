75d27893549f66e47aff4a3d11cc351987382333
PepperYan <188266760@qq.com>
2013-11-29 00:42:23 +0800

    not push to server

--- cube-android/assets/www/com.csair.deviceregist/index.js
+++ cube-android/assets/www/com.csair.deviceregist/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("deviceready", function(){
+    alert("ready");
 	//激活单选框
 	$("input[name=deviceSrc]").click();
 	//查询设备注册数据并填好
@@ -42,12 +43,13 @@ function fillId(id){
 }
 
 function queryAndFillDeviceInfo(){
-	console.log("查询注册信息");
+    alert("查询注册信息");
 	if(cordova && cordova.exec){
 		cordova.exec(
 			function(data){
-				console.log("查询成功");
+				alert("查询成功");
 				if(data){
+                    alert("data不为空");
                     //安卓是在这里如,传入object,ios是直接调用fillData
                     data = JSON.stringify(data);
                     $("#registInfo").html("您的设备已注册");
@@ -55,11 +57,12 @@ function queryAndFillDeviceInfo(){
 				    isUpdate = true;
 				}else{
 					$("#registInfo").html("您的设备未进行注册");
+                    alert("data为空");
 					isUpdate = false;
 				}
 			}, 
 			function(err) {
-				console.log("查询失败或者无数据");
+                alert("查询失败或者无数据");
                 $("#registInfo").html("您的设备未进行注册");
         	}
         , "DeviceRegister", "queryDevcieInfo", []);
